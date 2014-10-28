59a3f8835756e53236cd9635d2657cc1e21e57b3
Administrador <alumno.1095384@ies-azarquiel.es>
2014-10-28 21:37:04 +0100

    Punto de Partida

---
+++ Hola Mundoo/src/controlador/Principal.java
@@ -0,0 +1,14 @@
+package controlador;
+
+import modelo.Saluda;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Saluda saludo=new Saluda();
+		saludo.diHola("Jose");
+
+	}
+
+}
---
+++ Hola Mundoo/src/modelo/Saluda.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Saluda {
+
+	public Saluda() { // Esto se tiene que escribir
+	}
+		public void diHola(String nombre){ // Esto se tiene que escribir
+		System.out.println("Hola: " + nombre + " que caña...");
+		}
+	
+}
